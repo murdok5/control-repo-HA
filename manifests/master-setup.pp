469ceafb2850e5de05e91adb06bf1d1ee835a5ea
murdok5 <mike.smith@puppet.com>
2016-12-12 10:44:07 -0800

    Add profile for master

---
+++ manifests/master-setup.pp
@@ -0,0 +1,13 @@
+class master-setup {
+
+  node_group { 'PE Master':
+    ensure               => present,
+    environment          => 'production',
+    override_environment => false,
+    parent               => 'PE Infrastructure',
+    rule                 => ['or', ['=', 'name', $::clientcert]],
+    classes              => { \
+      'pe_repo::platform::el_7_x86_64'                   => {},
+      'pe_repo::platform::windows_x86_64'                => {},
+    },
+  }

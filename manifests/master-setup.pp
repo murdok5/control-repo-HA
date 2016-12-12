efddbb731179272d501eac973a5d1a75e75559c4
murdok5 <mike.smith@puppet.com>
2016-12-12 12:39:40 -0800

    update syntax issues

--- manifests/master-setup.pp
+++ manifests/master-setup.pp
@@ -1,4 +1,4 @@
-class master-setup {
+class reposetup {
 
   node_group { 'PE Master':
     ensure               => present,
@@ -6,8 +6,9 @@ class master-setup {
     override_environment => false,
     parent               => 'PE Infrastructure',
     rule                 => ['or', ['=', 'name', $::clientcert]],
-    classes              => { \
+    classes              => {
       'pe_repo::platform::el_7_x86_64'                   => {},
       'pe_repo::platform::windows_x86_64'                => {},
     },
   }
+}
